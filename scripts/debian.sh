50319e84859a64b8f4241aaef97785eddb47403b
hashiiiii <pkj59wwj@gmail.com>
2025-02-17 21:46:44 +0900

    improve: add some comments

--- scripts/debian.sh
+++ scripts/debian.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-# Exit on any error
 set -e
 
 # Error handling
 trap 'echo "Error occurred at line $LINENO. Previous command exited with status: $?"' ERR
 
-# Source required configuration and utility files
 source "$DOTFILE_HOME/lib/log.sh"
 source "$DOTFILE_HOME/lib/backup.sh"
+source "$DOTFILE_HOME/lib/font.sh"
 source "$DOTFILE_HOME/dotfiles.conf"
 
 logInfo 'Running debian.sh...'
 
-#
+############################################
 # System Package Installation
-#
+############################################
 
 # Update package list and install required packages
 logInfo "Updating package list..."
@@ -28,18 +27,18 @@ for pkg in "${APT_PACKAGES[@]}"; do
 done
 logOK "All required apt packages have been installed."
 
-#
+############################################
 # Homebrew Installation
-#
+############################################
 
 # Install Homebrew package manager (non-interactive mode)
 logInfo "Installing Homebrew..."
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
-#
+############################################
 # Common Setup and Fonts
-#
+############################################
 
 # Run common setup script for all platforms
 logInfo "Executing common.sh..."
@@ -47,13 +46,12 @@ logInfo "Executing common.sh..."
 
 # Install Nerd Fonts for terminal
 logInfo "Installing Nerd Fonts..."
-source "$DOTFILE_HOME/lib/font.sh"
 install_nerd_fonts "$NERD_FONT"
 logOK "Nerd Fonts installation completed."
 
-#
+############################################
 # Dotfiles Setup
-#
+############################################
 
 # Create symlinks for dotfiles with backup
 for file in "${DOTFILES[@]}"; do
@@ -61,9 +59,9 @@ for file in "${DOTFILES[@]}"; do
 done
 logOK "dotfiles for Debian installation completed!"
 
-#
+############################################
 # Shell Configuration
-#
+############################################
 
 # Save current shell and change to zsh
 DOTFILES_CACHE="${XDG_CACHE_HOME:-$HOME/.cache}/dotfiles"
