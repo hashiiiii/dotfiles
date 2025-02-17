d6f94c1d0866738f951022e2986f7dcc502c7be9
hashiiiii <pkj59wwj@gmail.com>
2025-02-17 21:27:32 +0900

    improve: add some comments

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,6 +4,7 @@ set -e
 
 source "$DOTFILE_HOME/lib/log.sh"
 
+# Prevent running as root for security
 if [ "$(id -u)" -eq 0 ]; then
   logErr "Please do not run this script as root. Run it as a regular user."
   exit 1
@@ -11,19 +12,22 @@ fi
 
 logInfo 'Running install.sh...'
 
+# Detect operating system
 OS=$(uname -s)
 
+# Install packages based on OS type
 if [[ "$OS" == "Linux" ]]; then
+  # For Linux, check if it's Debian-based
   source /etc/os-release
   if [ "$ID" = "debian" ] || echo "$ID_LIKE" | grep -qi "debian"; then
     logOK 'Debian-based system is detected.'
     "$DOTFILE_HOME/scripts/debian.sh"
   else
-    logErr "Other Linux distribution: $ID"
+    logErr "Unsupported Linux distribution: $ID"
     exit 1
   fi
 elif [[ "$OS" == "Darwin" ]]; then
-  logOK "MacOSX is detected."
+  logOK "MacOS is detected."
   "$DOTFILE_HOME/scripts/macosx.sh"
 else
   logErr "Unsupported OS: $OS"
